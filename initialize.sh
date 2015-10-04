24099722880e10f12125ed9170ad021f30df6213
George Lifchits <george.lifchits@gmail.com>
2015-10-04 15:56:24 -0400

    Check for brew in the initialize script, install brew cask

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+
+if brew -v >/dev/null 2>&1; then
+  echo "brew is installed :)"
+else
+  echo "Install brew first!"
+  exit
+fi
+
+echo "Symlinking files"
 for pth in .vim .zshrc .vimrc .jshintrc .gitconfig
 do
   ln -s "$(pwd)/$pth" ~/"$pth"
 done
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/fonts
